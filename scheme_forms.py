cdec06d98a8341b3bad7f6b182bd0e11129854c9
Ezra Lamoreaux <65139116+VoicefulEmu@users.noreply.github.com>
2023-04-13 15:39:56 -0600

    solve 10

--- scheme_forms.py
+++ scheme_forms.py
@@ -42,9 +42,13 @@ def do_define_form(expressions, env):
         
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
-        # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
-        "*** YOUR CODE HERE ***"
+        name = signature.first
+        formals = signature.rest
+        body = expressions.rest
+        new_instance = do_lambda_form(Pair(formals, body), env)
+        env.bindings[name] = new_instance
+        return name
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
