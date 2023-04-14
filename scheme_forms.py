6630d720f20b009a7df67a5c7243fff4df902af4
Ezra Lamoreaux <65139116+VoicefulEmu@users.noreply.github.com>
2023-04-14 14:53:34 -0600

    solve problem 14 finish part 2

--- scheme_forms.py
+++ scheme_forms.py
@@ -229,9 +229,14 @@ def make_let_frame(bindings, env):
         raise SchemeError('bad bindings list in let form')
     names = values = nil
     # BEGIN PROBLEM 14
-    "*** YOUR CODE HERE ***"
-    # END PROBLEM 14
+    while bindings != nil:
+        validate_form(bindings.first,2,2)
+        names = Pair(bindings.first.first, names)
+        values = Pair(scheme_eval(bindings.first.rest.first,env), values)
+        bindings = bindings.rest
+    validate_formals(names)
     return env.make_child_frame(names, values)
+        # END PROBLEM 14
 
 
 def do_define_macro(expressions, env):
